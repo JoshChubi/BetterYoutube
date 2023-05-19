a11f28d75094596911d7ea4cbb02e5532767623e
SethOrtiz <wobbxseth@gmail.com>
2023-05-18 21:51:52 -0700

    Added Video Player Support

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './components/header/Header.js';
 import FilterScrollbar from './components/scrollbar/FilterScrollbar.js';
 import HomeContent from './components/content/HomeContent.js';
 
+import video_obj from './videos/absolute_in_doubt.mp4' /*TO-DO: make dynamic - pull from s3 bucket*/
+
 function App() {
     return (
        <div className="App">
@@ -18,6 +20,9 @@ function App() {
 
             <div className="Content-Container">
                 <HomeContent />
+                <div className="video-container">
+                    <video src={video_obj} controls></video>
+                </div>
             </div>
        </div>
   );
