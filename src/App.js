fe58fac269526625c0a2148e97c963ead7867d8d
Josh Ortiz <Ortizjb0961@gmail.com>
2023-05-24 00:23:49 -0700

    Added Rerendering Depending on State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import './Colors.css';
 
+import { useState } from 'react';
 import Header from './components/header/Header.js';
 import FilterScrollbar from './components/scrollbar/FilterScrollbar.js';
 import HomeContent from './components/content/HomeContent.js';
 import VideoPlayerContent from './components/content/VideoPlayerContent';
 
 function App() {
+    const [contentData, setContent] = useState({Type:"Home"});
+
+    function ChooseContent() {
+        if (contentData.Type === "Home")
+            return <HomeContent />;
+        else if (contentData.Type === "VideoPlayer")
+            return <VideoPlayerContent title=" Doing It Wrong - Capoxxo" author="Capoxxo" likes="200" views="1.3mil" date="1/1/2020" desc="This is a video of two friends making great music together!" />;
+    }
+
     return (
        <div className="App">
             <div className="Header-Container">
@@ -14,7 +24,7 @@ function App() {
             </div>
 
             <div className="Content-Container">
-                <VideoPlayerContent title=" Doing It Wrong - Capoxxo" author="Capoxxo" likes="200" views="1.3mil" date="1/1/2020" desc="This is a video of two friends making great music together!" />
+                {ChooseContent()}
             </div>
 
             {/*<div className="Filter-Container">
