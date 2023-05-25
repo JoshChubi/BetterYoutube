fc113a1841cbfc52a4dff12cbf26b237d4e79d41
SethOrtiz <wobbxseth@gmail.com>
2023-05-25 03:11:22 -0700

    Integrated Lambda Function to get Video Meta Data for Thumbnails and Player

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import './App.css';
 import './Colors.css';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Header from './components/header/Header.js';
 import FilterScrollbar from './components/scrollbar/FilterScrollbar.js';
 import HomeContent from './components/content/HomeContent.js';
 import VideoPlayerContent from './components/content/VideoPlayerContent';
 
 function App() {
-    const [contentData, setContentData] = useState({ type: "Home", etag: null, title: null, author: null, likes: null, views: null, date: null, desc: null });
+    const [contentData, setContentData] = useState({ type: null, etag: null, title: null, author: null, likes: null, views: null, date: null, desc: null });
+    let refVideoMetadata = useRef(null);
+
+    async function getVideosMetadata()
+    {
+        const url = "https://ahqbw4sfkd.execute-api.us-west-1.amazonaws.com/HTTGetUserRecommendedEtags?userId=123456";
+        const response = await fetch(url);
+        const metaDataObj = await response.json();
+
+        var refMetadata = {}
+
+        metaDataObj.map((metadata) => {
+            refMetadata[metadata.Item.Etag] = {
+                title: metadata.Item.VideoName,
+                author: metadata.Item.AuthorName,
+                likes: metadata.Item.Likes,
+                views: metadata.Item.Views,
+                date: metadata.Item.PublishDate,
+                desc: metadata.Item.Description,
+            }
+        });
+
+        refVideoMetadata.current = refMetadata;
+        setContentData({type: "Home"});
+    }
 
     function ChooseContent() {
         if (contentData.type === "Home")
-            return <HomeContent setContentData={setContentData} />;
+            return <HomeContent setContentData={setContentData} refVideoMetadata={refVideoMetadata} />;
         else if (contentData.type === "VideoPlayer")
             return <VideoPlayerContent etag={contentData.etag} title={contentData.title} author={contentData.author} likes={contentData.likes} views={contentData.views} date={contentData.date} desc={contentData.desc} />;
+        else if (contentData.type === null)
+            getVideosMetadata();
     }
     return (
        <div className="App">
--- src/components/content/HomeContent.js
+++ src/components/content/HomeContent.js
@@ -6,34 +6,30 @@ import App from '../../App.js';
 
 function HomeContent(props) {
 
-    async function getVideoData(etag) {
-
-        const url = "https://ahqbw4sfkd.execute-api.us-west-1.amazonaws.com/HTTPGetVideoMetaData?Etag=" + etag;
-        const response = await fetch(url);
-        const data = await response.json();
-
-        return data;
-    }
-
-    async function PlayVideo(etag) {
-
-        const videoData = (await getVideoData(etag)).Item;
-
-        var dateObj = new Date(videoData.PublishDate * 1000);
+    async function PlayVideo(etag, metadata) {
+        var dateObj = new Date(metadata.date * 1000);
         var utcString = dateObj.toUTCString();
         var dateValues = utcString.split(' ');
         utcString = dateValues[1] + " " + dateValues[2] + " " + dateValues[3];
 
-        props.setContentData({ type: "VideoPlayer", etag: etag, title: videoData.VideoName, author: videoData.AuthorName, likes: videoData.Likes, views: videoData.Views, date: utcString, desc: videoData.Description });
+        props.setContentData({ type: "VideoPlayer", etag: etag, title: metadata.title, author: metadata.author, likes: metadata.likes, views: metadata.views, date: utcString, desc: metadata.desc });
+    }
+
+    function CreateGridThumbnail(etag, metadata) {
+        return (<div className="grid-item">
+            <HomeThumbnail src={"https://dqdow3mh2lvus.cloudfront.net/thumbnails/" + etag + ".png"} alt={metadata.title} onClick={() => PlayVideo(etag, metadata)} />
+        </div>)
     }
 
     return (
         <div className="homeContent">
             <div className="homeContent-Scroll">
-                <div className="homeContent-Grid">
-                    <div className="grid-item">
-                        <HomeThumbnail src={ThumbnailTest} alt="Video 1" onClick={() => PlayVideo("29b1e39a561b75a75874fb2491f53dad") } />
-                    </div>
+                <div className="homeContent-Grid" >
+                    {
+                        Object.entries(props.refVideoMetadata.current).map(([etag, metadata]) => {
+                            return CreateGridThumbnail(etag, metadata);
+                        })   
+                    }
                 </div>
             </div>
         </div>
