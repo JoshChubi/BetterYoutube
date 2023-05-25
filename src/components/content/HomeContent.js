f12e9f1dce9b959af887fd42a64a4a0f027d8029
SethOrtiz <wobbxseth@gmail.com>
2023-05-24 23:56:50 -0700

    integrated aws w/ videoPlayer content - mapping

--- src/components/content/HomeContent.js
+++ src/components/content/HomeContent.js
@@ -5,8 +5,26 @@ import HomeThumbnail from '../thumbnail/HomeThumbnail.js';
 import App from '../../App.js';
 
 function HomeContent(props) {
-    function PlayVideo(etag) {
-        props.setContentData({ type: "VideoPlayer", title: "Doing It Wrong - Capoxxo", author: "Capoxxo", likes: "200", views: "1.3mil", date: "1/1/2020", desc: "This is a video of two friends making great music together!" });
+
+    async function getVideoData(etag) {
+
+        const url = "https://ahqbw4sfkd.execute-api.us-west-1.amazonaws.com/HTTPGetVideoMetaData?Etag=" + etag;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return data;
+    }
+
+    async function PlayVideo(etag) {
+
+        const videoData = (await getVideoData(etag)).Item;
+
+        var dateObj = new Date(videoData.PublishDate * 1000);
+        var utcString = dateObj.toUTCString();
+        var dateValues = utcString.split(' ');
+        utcString = dateValues[1] + " " + dateValues[2] + " " + dateValues[3];
+
+        props.setContentData({ type: "VideoPlayer", title: videoData.VideoName, author: videoData.AuthorName, likes: videoData.Likes, views: videoData.Views, date: utcString, desc: videoData.Description });
     }
 
     return (
@@ -14,7 +32,7 @@ function HomeContent(props) {
             <div className="homeContent-Scroll">
                 <div className="homeContent-Grid">
                     <div className="grid-item">
-                        <HomeThumbnail src={ThumbnailTest} alt="Video 1" onClick={PlayVideo} />
+                        <HomeThumbnail src={ThumbnailTest} alt="Video 1" onClick={() => PlayVideo("29b1e39a561b75a75874fb2491f53dad") } />
                     </div>
                 </div>
             </div>
--- src/components/content/VideoPlayerContent.js
+++ src/components/content/VideoPlayerContent.js
@@ -4,6 +4,7 @@ import './VideoPlayerContent.css';
 import video_obj from './video_obj.mp4';
 
 function VideoData(props) {
+
     return (
         <div className="videoData">
             <div className="videoData-Information">
@@ -18,29 +19,7 @@ function VideoData(props) {
         );
 };
 
-async function getVideoData() {
-
-    const url = "https://ahqbw4sfkd.execute-api.us-west-1.amazonaws.com/HTTPGetVideoMetaData?Etag=29b1e39a561b75a75874fb2491f53dad";
-
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("data inside getVideoData() - stringify(): " + JSON.stringify(data) + "\n");
-    console.log("data.Item.VideoName inside getVideoData(): " + data.Item.VideoName + "\n");
-
-    return data;
-}
-
 function VideoPlayerContent(props) {
-
-    const data = getVideoData()
-        .then(response => {
-            response.json();
-        })
-        .then(data => {
-            console.log("data inside VideoPlayerContent() .then: " + data.Item.VideoName);
-        })
-
-    console.log("data inside VideoPlayerContetn(): " + data + "\n")
 
     return (
         <div className="videoPlayerContent">
